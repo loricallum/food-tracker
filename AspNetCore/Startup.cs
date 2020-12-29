14bf673c29549e786996de91d13d48dd617d9e0d
LLewis <lorimlewis@gmail.com>
2020-12-29 20:05:27 +0000

    Add files via upload

--- AspNetCore/Startup.cs
+++ AspNetCore/Startup.cs
@@ -57,15 +57,13 @@ namespace AspNetCore
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationDbContext dbContext)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext dbContext)
         {
-            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
-
+            if (env.IsDevelopment()) 
+            {
+                app.UseDeveloperExceptionPage();
+            }
             dbContext.Database.EnsureCreated();
-
-            app.UseCors("VueCorsPolicy");
-
-            app.UseAuthentication();
 
             app.UseMvc();
         }
